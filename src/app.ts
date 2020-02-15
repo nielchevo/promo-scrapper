a069ff8c95ade276ae61746ad4eba772b9124b9a
Nathaniel Gunawan <nielchvo@gmail.com>
2020-02-15 23:00:46 +0700

    add click handle

--- src/app.ts
+++ src/app.ts
@@ -10,25 +10,47 @@ export class App {
    * run - run the app
    */
   public async run() {
-    const browser = await puppeteer.launch();
-    console.log('initialize app...');
-
-    const page = await browser.newPage();
-    await page.goto('https://www.bankmega.com/promolainnya.php#');
+    try{
+      const browser = await puppeteer.launch();
+      console.log('initialize app...');
+  
+      const page = await browser.newPage();
+      await page.setViewport({width: 1080, height: 1920});
+      await page.goto('https://www.bankmega.com/promolainnya.php#');
+      
+      if (!FileManager.IfExist()) {
+        let htmlBody = await page.content();
+        FileManager.SaveToFile(htmlBody);
+      }
+  
+      const subPromo = await page.$$eval("#contentpromolain2 div#subcatpromo div img", e => {
+        return e.map(i => {
+          return i;
+        });
+      })
+      console.log(subPromo);
     
-    if (!FileManager.IfExist()) {
-      let htmlBody = await page.content();
-      FileManager.SaveToFile(htmlBody);
+      // on click handle
+      const ds = await page.$('img#travel');
+        ds?.click().then( ()=> console.log("done !"));
+      
+      await page.waitForSelector("#contenpromolain2 ", { timeout: 3000});
+      await page.screenshot({ path: './images.jpeg', type: "jpeg"});
+
+      // const x = await (await page.waitForSelector("table.tablepaging")).$$eval("#contentpromolain2 table.tablepaging tr td a", el => { 
+      //     return el.map(e => {
+      //       if(e.hasAttribute('id')) {
+      //         return e.outerHTML;
+      //       }
+      //     });
+      // });
+      // console.log(x);
+
+      await page.close();
+      await browser.close();
+    } catch (error) {
+      if (error) console.log(error);
     }
-
-    // begin scraping 
-    // narrow down element, get contentpromolain2
-    const rootElement = await page.$eval('#contentpromolain2', (element) => {
-      return element.outerHTML;
-    });
-    console.log(rootElement);
-
-    await browser.close();
   }
 }
 
