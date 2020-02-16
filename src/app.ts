4a8c02ff7b2bbcae10622157e46cee7050df4bbc
Nathaniel Gunawan <nielchvo@gmail.com>
2020-02-17 00:29:51 +0700

    add scrap promo detail

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
 import puppeteer from 'puppeteer';
 
 import { FileManager } from "./FileManager";
+import { ResolveOptions } from 'dns';
 
 export class App {
-
+  
   private promoThumbnail:Array<string> = [];
-
-  constructor() { }
+  private mMainURL = "https://www.bankmega.com/";
+  
+  constructor() {}
 
   /**
    * run - run the app
    */
   public async run() {
     try{
-      const browser = await puppeteer.launch();
+      // const browser = await puppeteer.launch();
       console.log('initialize app...');
-  
+      const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.setViewport({width: 1080, height: 1920});
-      await page.goto('https://www.bankmega.com/promolainnya.php#');
+      // await page.setViewport({width: 1080, height: 1920});
+      await page.goto(this.mMainURL+"promolainnya.php#");
       
       if (!FileManager.IfExist()) {
         let htmlBody = await page.content();
@@ -48,17 +50,49 @@ export class App {
         });
       }
       
-      // get promo detail 
+      // Get all of Promo detail url link
       const promoThumbnail = await page.$$eval("#promolain li a", (el) => {
         return el.map((e) => {
           return e.getAttribute('href');
         });
       });
 
-      promoThumbnail.forEach(element => {
-        console.log(`Begin new page ${element?.toString()}`);
-      });  
+      // Begin click promo thumbnail and scrape data
+      for (const urlLink of promoThumbnail) {
+        let link = urlLink?.toString();
+        
+        let promoDetailPage = await browser.newPage();
+        await (async () => {
+          console.log(`Begin new page ${link?.toString()}`);
+          let urlDetail = this.mMainURL+link?.toString();
+          console.log('url detail : ', urlDetail);
+          await promoDetailPage.goto(urlDetail, {waitUntil: "load"})
+            .then(e => console.log(`goto response: ${e?.status}`))
+            .catch(e => console.log(e));
 
+          // scrap promo detail page
+          const data = await promoDetailPage.evaluate(()=> { 
+            let title = document.querySelector('div.titleinside > h3:nth-child(1)')?.innerHTML;
+            let area = document.querySelector('.area > b:nth-child(1)')?.innerHTML;
+            let dateStart = document.querySelector('.periode > b:nth-child(1)')?.innerHTML;
+            let dateEnd = document.querySelector('.periode > b:nth-child(2)')?.innerHTML;
+            
+            const scrapData = { 
+              title: title, 
+              area: area,
+              periodeBegin: dateStart, 
+              periodeEnd: dateEnd 
+            };
+
+            return scrapData;
+          });
+
+          console.log(`our scraped data: ${JSON.stringify(data)}`);
+        })().then( async () => {
+          console.log(`Done looping page ${link}. we can close this page`);
+          await promoDetailPage.close();
+        })
+      }
       console.log(promoThumbnail);
       
       await page.close();
@@ -66,10 +100,6 @@ export class App {
     } catch (error) {
       if (error) console.log(error);
     }
-  }
-
-  private scrapeNewPage() {
-    
   }
 }
 
