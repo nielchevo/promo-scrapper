8622c49727192c72d980d5f6661808fdf7a54e7c
Nathaniel Gunawan <nielchvo@gmail.com>
2020-02-17 05:08:57 +0700

    change name

--- src/Solution.ts
+++ src/Solution.ts
@@ -2,16 +2,16 @@ import puppeteer from 'puppeteer';
 
 import { FileManager } from "./FileManager";
 import { ResolveOptions } from 'dns';
+import { IpcNetConnectOpts } from 'net';
+import { stringify } from 'querystring';
 
-export class App {
-  
-  private mMainData:PromoCategory; 
+export class Solution {
   
   private mMainURL = "https://www.bankmega.com/";
   private DELAY_CLICK = 4000;
-  
+  private solutionJSON: Array<Object> = [];
+
   constructor() {
-    
   }
 
   /**
@@ -23,15 +23,10 @@ export class App {
       console.log('initialize app...');
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      
+
       // Open promo page
       await page.goto(this.mMainURL+"promolainnya.php#");
-      
-      // if (!FileManager.IfExist()) {
-      //   let htmlBody = await page.content();
-      //   FileManager.SaveToFile(htmlBody);
-      // }
-  
+    
       const promoCategory = await page.$$eval("#contentpromolain2 div#subcatpromo div img", e => {
         return e.map(i => {
           return `img#${i.getAttribute('id')}`;
@@ -43,24 +38,23 @@ export class App {
       for (const promo of promoCategory) {
         
         console.info(`--> Current category: ${promo.toString()}`);
-
         
-
         // Simulate click promo to refresh page.
         const ds = await page.$(promo.toString());
         ds?.click().then( ()=> console.log("done !"));
 
         await page.waitFor(1500); // wait delay page to load
         
-        const pagination = await page.$$eval("#contentpromolain2 table.tablepaging tr td a", el => { 
+        const data = await page.$$eval("#contentpromolain2 table.tablepaging tr td a", el => { 
           // Eliminate goto: first and last in pagination
           const pageIndex = el.filter(e => e.hasAttribute('id'));
           return pageIndex.map(e => e.getAttribute('title')?.toString());
         })
         .then( async (pageLength) => {
-
+          let dataList:Array<Object> = []; 
           // Begin looping pagination
           for (const it of pageLength) {
+
             await (async ()=> {
               console.log(`pagination: ${it}`);
               const ds = await page.$(`[title="${it}"]`);
@@ -107,68 +101,30 @@ export class App {
                     });
 
                     console.log(`our scraped data: ${JSON.stringify(data)}`);
+                    dataList.push(data);
+
                   })().then( async () => {
                     console.log(`Done looping page ${link}. we can close this page`);
                     await promoDetailPage.close();
                   })
                 }
                 console.log(promoThumbnail);
-
               })
             })().then( async ()=> {
               console.log('on done scrape thumbnail');
+              
             });
           }
-        });
-      }
-      
-      // Get all of Promo detail url link
-      // const promoThumbnail = await page.$$eval("#promolain li a", (el) => {
-      //   return el.map((e) => {
-      //     return e.getAttribute('href');
-      //   });
-      // });
-
-      // Begin click promo thumbnail and scrape data
-      // for (const urlLink of promoThumbnail) {
-      //   let link = urlLink?.toString();
+          return dataList;
+        })
         
-      //   let promoDetailPage = await browser.newPage();
-      //   await (async () => {
-      //     console.log(`Begin new page ${link?.toString()}`);
-      //     let urlDetail = this.mMainURL+link?.toString();
-      //     console.log('url detail : ', urlDetail);
-      //     await promoDetailPage.goto(urlDetail, {waitUntil: "load"})
-      //       .then(e => console.log(`goto response: ${e?.status}`))
-      //       .catch(e => console.log(e));
+        this.solutionJSON.push({category: promo, data: data});
+      }
 
-      //     // scrap promo detail page
-      //     const data = await promoDetailPage.evaluate(()=> { 
-      //       let title = document.querySelector('div.titleinside > h3:nth-child(1)')?.innerHTML;
-      //       let area = document.querySelector('.area > b:nth-child(1)')?.innerHTML;
-      //       let dateStart = document.querySelector('.periode > b:nth-child(1)')?.innerHTML;
-      //       let dateEnd = document.querySelector('.periode > b:nth-child(2)')?.innerHTML;
-            
-      //       const scrapData = { 
-      //         title: title, 
-      //         area: area,
-      //         periodeBegin: dateStart, 
-      //         periodeEnd: dateEnd 
-      //       };
-
-      //       return scrapData;
-      //     });
-
-      //     console.log(`our scraped data: ${JSON.stringify(data)}`);
-      //   })().then( async () => {
-      //     console.log(`Done looping page ${link}. we can close this page`);
-      //     await promoDetailPage.close();
-      //   })
-      // }
-      // console.log(promoThumbnail);
-      
       await page.close();
       await browser.close();
+
+      FileManager.SaveToFile(JSON.stringify(this.solutionJSON).toString());
     } catch (error) {
       if (error) console.log(error);
     }
@@ -176,5 +132,5 @@ export class App {
 }
 
 // Start app
-const tes = new App();
+const tes = new Solution();
 tes.run();
